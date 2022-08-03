395683e3b7d5f5b79df6bfeae607c0d182de1f86
SourabhK0625 <106949506+SourabhK0625@users.noreply.github.com>
2022-08-03 12:28:23 +0530

    Inbox Page

--- src/components/WelcomePage/mailbox/CreateEmail.js
+++ src/components/WelcomePage/mailbox/CreateEmail.js
@@ -25,8 +25,20 @@ const CreateEmail = () => {
       heading: emailHeadingInp.current.value,
       body: draftToHtml(convertToRaw(editorState.getCurrentContent())),
     };
+    const cleanToEmail = toEmailInp.current.value.replace(/[^a-zA-Z ]/g, "");
+    fetch(`https://e-mail-client-box-default-rtdb.firebaseio.com/${cleanToEmail}/inbox.json`,{
+      method: 'POST',
+      headers: {
+        "Content-type": "application-json",
+      },
+      body: JSON.stringify(emailData),
+    })
+    .then((res=>res.json()))
+    .then((data)=> {
+      // console.log(data);
+    })
 
-    fetch(`https://e-mail-client-box-default-rtdb.firebaseio.com/${cleanUserEmail}/sentemails.json`, {
+    fetch(`https://mailboxclient-default-rtdb.firebaseio.com/${cleanUserEmail}/sentemails.json`, {
       method: "POST",
       headers: {
         "Content-type": "application-json",
